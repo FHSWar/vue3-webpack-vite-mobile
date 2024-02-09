561e4859ee0a76a0a2bd084411655225aecde5fb
杜金华 <abc>
2024-02-09 14:20:32 +0800

    feat: 新增anls命令

--- build/webpack.prod.cjs
+++ build/webpack.prod.cjs
@@ -11,19 +11,21 @@ const plugins = [
   new MiniCssExtractPlugin({
     filename: 'css/[name].[contenthash].css',
     chunkFilename: 'css/[name].[contenthash].css'
-  }),
-  new BundleAnalyzerPlugin({
-    analyzerMode: 'server',
-    analyzerHost: '127.0.0.1',
-    analyzerPort: 8888,
-    reportFilename: 'report.html',
-    defaultSizes: 'parsed',
-    generateStatsFile: false,
-    statsFilename: 'stats.json',
-    statsOptions: null,
-    logLevel: 'info'
   })
 ]
+
+if (process.env.ANALYZE) {
+  plugins.push(
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'server',
+      analyzerHost: 'localhost',
+      reportFilename: 'report.html',
+      defaultSizes: 'gzip',
+      generateStatsFile: false,
+      logLevel: 'info'
+    })
+  )
+}
 
 module.exports = merge(common, {
   optimization: {
